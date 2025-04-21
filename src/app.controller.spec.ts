4165ab31eee8cc056e76ee9db134124fcabd48ce
Cauê Lima <cauesvlima@gmail.com>
2025-04-21 14:48:23 -0300

    adicionando pastas

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './modules/app-root/app.controller';
+import { AppService } from './modules/app-root/service/app.service';
 
 describe('AppController', () => {
   let appController: AppController;
--- src/modules/app-root/app.module.ts
+++ src/modules/app-root/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppService } from './service/app.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
